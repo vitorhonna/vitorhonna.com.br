94f5d31eb005801a88d67990f5e47741046ea281
Vitor Honna <vitorhonna@gmail.com>
2023-12-05 00:32:41 +0000

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Jucucu, vc olhou!</h1>
+  return (
+    <div>
+      <h1>Jucucu, vc olhou!</h1>
+      <img
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr7SKaEgguQ7cMCDX5Mk8az5Q-uf5Yv2Fj9tcG5xHV1I0WQZNH3o9jKPhBWiackAQv7yI&usqp=CAU'
+        alt='cat'
+      />
+    </div>
+  );
 }
 
 export default Home;
