cdf350e78e3b231e21a7ce5e83498dda4b9eafbc
Vitor Honna <vitorhonna@gmail.com>
2023-12-06 15:53:57 +0000

    Update homapage to automatically flip the image

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Home() {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+  useEffect(() => {
+    // Automatically change the image after 2 seconds
+    const timeoutId = setTimeout(() => {
+      setIsFlipped(!isFlipped);
+    }, 2000);
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+    // Clear the timeout when the component unmounts or when isFlipped changes
+    return () => clearTimeout(timeoutId);
+  }, [isFlipped]);
+
+  const imagePath = isFlipped
+    ? 'https://raw.githubusercontent.com/vitorhonna/vitorhonna.com.br/main/imgs/cat.jpeg?token=GHSAT0AAAAAACKC4QW2G2V33YOKUNZ4OTK2ZLQUP5Q'
+    : 'https://raw.githubusercontent.com/vitorhonna/vitorhonna.com.br/main/imgs/jucucu.png?token=GHSAT0AAAAAACKC4QW3GFEWN3KW6LBYFYWOZLQUQLQ';
 
   return (
     <div>
       <h1>Jucucu, vc olhou!</h1>
-      <img
-        src={
-          isHovered
-            ? 'https://raw.githubusercontent.com/vitorhonna/vitorhonna.com.br/main/imgs/cat.jpeg?token=GHSAT0AAAAAACKC4QW2G2V33YOKUNZ4OTK2ZLQUP5Q'
-            : 'https://raw.githubusercontent.com/vitorhonna/vitorhonna.com.br/main/imgs/jucucu.png?token=GHSAT0AAAAAACKC4QW3GFEWN3KW6LBYFYWOZLQUQLQ'
-        }
-        alt='image'
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      />
+      <img src={imagePath} alt='image' />
     </div>
   );
 }
